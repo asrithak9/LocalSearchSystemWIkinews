207a929c532e666e3be26d39c2aad6b508b52997
Asritha Kondaveeti <asrithakondaveeti@gmail.com>
2020-06-09 00:30:17 -0500

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Courses\5712-Networked Data Modelling and Processing\LocalSearchSystemWikinews\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
